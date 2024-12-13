6f90ca3eb5d63016b8de11ec1ca2e343eb32876d
Ondřej Hložek <hlozek.o1@gmail.com>
2024-12-14 00:05:07 +0100

    feat: python import configuration

---
+++ aeon_demo/__init__.py
@@ -0,0 +1,5 @@
+from .aeon_demo import *
+
+__doc__ = aeon_demo.__doc__
+if hasattr(aeon_demo, "__all__"):
+    __all__ = aeon_demo.__all__
---
+++ aeon_demo/main.py
@@ -0,0 +1,4 @@
+import aeon_demo
+
+result = aeon_demo.sum_as_string(1, 2)
+print(result)
