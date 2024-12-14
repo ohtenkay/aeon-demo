a4490584a82fe4de3c736a3767c31f32e117ad7b
Ondřej Hložek <hlozek.o1@gmail.com>
2024-12-14 22:41:58 +0100

    feat: python stubs plus importing boolean network

--- aeon_demo/main.py
+++ aeon_demo/main.py
@@ -1,4 +1,9 @@
-import aeon_demo
+from biodivine_aeon import BooleanNetwork
 
-result = aeon_demo.sum_as_string(1, 2)
+from aeon_demo import *
+
+boolean_network = BooleanNetwork.from_file("edition-2022-aeon/001.aeon")
+print(boolean_network)
+
+result = sum_as_string(1, 2)
 print(result)
